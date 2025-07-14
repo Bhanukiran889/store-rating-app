4803bb5f2cdb0da785cf32669195ad2286de1126
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-07-14 10:01:17 +0530

    enhance fserver.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://store-rating-6j49s2564-bhanukiran-reddys-projects.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://store-rating-app-kappa.vercel.app/stores'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
